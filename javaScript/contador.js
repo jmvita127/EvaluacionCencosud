00895b3c3622df91b7bbcf525b4d197ee3ef915c
jmvita127 <jmvita127@gmail.com>
2019-08-16 19:06:00 -0300

    Optimizacion de codigo y correcciones responsive

--- javaScript/contador.js
+++ javaScript/contador.js
@@ -1,6 +1,6 @@
 // Variables Globales
 var tiempo = {};
-var reloj = new Date("2016-06-01 5:00:00 PM"); // Obtener la fecha y almacenar en reloj
+var reloj = new Date("2019-06-01 24:00:00"); // Obtener la fecha y almacenar en reloj
 var intervalo = window.setInterval(mostrar_hora, 1); // Frecuencia de actualización
 var i = 0; // Esta variable va a definir los estados de intervalo
 
@@ -9,7 +9,7 @@ function mostrar_hora()
     var tiempoActual = new Date();
     // Inserta la hora almacenada en reloj en el span con id hora
     tiempo.horas = document.getElementById('hora');
-    tiempo.horas.innerHTML = reloj.getHours() - tiempoActual.getHours();
+    tiempo.horas.innerHTML = reloj.getHours() + tiempoActual.getHours();
 
     // Inserta los minutos almacenados en reloj en el span con id minuto
     tiempo.minuto = document.getElementById('minuto');
